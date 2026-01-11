01e18fc1cd69637a138f422192291f9316d16399
V1HSnik74 <xboxfan200674@gmail.com>
2026-01-11 23:58:04 +0500

    Dog breeds added to db from API

--- dogbreeds/settings.py
+++ dogbreeds/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = ['VHSnik74.pythonanywhere.com', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
-    'main',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main'
 ]
 
 MIDDLEWARE = [
---
+++ main/management/commands/breedsloader.py
@@ -0,0 +1,41 @@
+from django.core.management import BaseCommand
+import requests
+from main.models import DogBreed
+
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument('--key', type=str, required=True)
+
+    def handle(self, *args, **options):
+        key = options['key']
+        offset = 0
+        while True:
+            response = requests.get(
+                'https://api.api-ninjas.com/v1/dogs',
+                headers={'X-Api-Key': key},
+                params={
+                    'min_weight': 1,
+                    'offset': offset
+                },
+                verify=False
+            )
+            if response.status_code != 200:
+                break
+            dogs = response.json()
+            if not dogs:
+                break
+            for dog in dogs:
+                DogBreed.objects.update_or_create(
+                    name=dog['name'],
+                    min_weight=dog['min_weight_male'],
+                    max_weight=dog['max_weight_male'],
+                    shedding=dog['shedding'],
+                    grooming=dog['grooming'],
+                    coat_length=dog['coat_length'],
+                    energy=dog['energy'],
+                    trainability=dog['trainability'],
+                    good_with_children=dog['good_with_children'],
+                    good_with_other_dogs=dog['good_with_other_dogs']
+                )
+            offset += 20
